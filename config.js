ead518874f29f122d2029a57d5762b7e154e7262
yuishiro1001 <86756268+yuishiro1001@users.noreply.github.com>
2021-07-01 10:46:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Đức à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao bé cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hí <3',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'thực ra tao vẫn yêu mày nhiều lắm bé ạ',
-    btnNo: 'mày thôi đi tao với mày giờ chỉ là bạn bè thôi',
-    question:'Trên thế giới hơn 7 tỉ người mà sao mày lại iu tau zậy <3',
-    btnReply: 'gửi nè <3',
-    reply: 'vì yêu mà anh cứ đâm đầu <33333333',
-    mess: 'tau biết mà 🥰. love diu',
-    messDesc: 'Tối nay call tắm cho xem ',
-    btnAccept: 'duyệttttt',
+    introTitle: 'hi guys',
+    introDesc: `in the world have lots of idol
+    but i don't know why i'm so inlove with Dyn
+    especially Dynastic
+    love you guys so much`,
+    btnIntro: 'click here',
+    title: 'you guys are such a lovely fandom 😙',
+    desc: 'do you love me ',
+    btnYes: 'opo! mahal kita',
+    btnNo: 'hindi',
+    question:'tell me why you guys love me ^^',
+    btnReply: 'here my cutiest ate',
+    reply: 'because you are so cute and look younger than your age',
+    mess: 'i know it ^^ you guys always said that to me ^^',
+    messDesc: ' can i sing for you guys tonight? ',
+    btnAccept: ' pwede!!!! i love it ',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
